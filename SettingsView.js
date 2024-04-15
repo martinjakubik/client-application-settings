7d774c850a728824983db4c1a202cf5e26237984
martin jakubik <martin.jakubik@sap.com>
2024-04-15 18:20:52 +0200

    first commit

---
+++ SettingsView.js
@@ -0,0 +1,18 @@
+import { createDiv } from "learnhypertext";
+import * from "./lib/learnhypertext.mjs";
+
+class SettingsView {
+    static render(aSettings) {
+        const oSettingsList = createDiv('settings');
+        aSettings.forEach((oSetting, nIndex) => {
+            console.log(`key: ${oSetting.key} value: ${oSetting.value}`);
+            const oSettingItem = createDiv(`settingItem${nIndex}`, oSettingsList);
+            const sSettingKey = oSetting.key;
+            const sSettingValue = oSetting.value;
+            createDiv(`settingKey${nIndex}`, oSettingItem);
+            createDiv(`settingValue${nIndex}`, oSettingItem);
+        });
+    }
+}
+
+export { SettingsView };
