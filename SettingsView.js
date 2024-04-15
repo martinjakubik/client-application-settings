5eb65e3bf6109a114a6a84c2df8550ee960df212
martin jakubik <martin.jakubik@gmail.com>
2024-04-15 20:54:01 +0200

    updated ids

--- SettingsView.js
+++ SettingsView.js
@@ -8,8 +8,8 @@ class SettingsView {
             const oSettingItem = createDiv(`settingItem${nIndex}`, oSettingsList);
             const sSettingKey = oSetting.key;
             const sSettingValue = oSetting.value;
-            createDiv(`settingKey${nIndex}`, oSettingItem);
-            createDiv(`settingValue${nIndex}`, oSettingItem);
+            createDiv(`settingKey-${sSettingKey}`, oSettingItem);
+            createDiv(`settingValue-${sSettingValue}`, oSettingItem);
         });
     }
 }
